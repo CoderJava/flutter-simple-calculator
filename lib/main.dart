4540ec4a734c1830fad82e46bbfb702686a0b3b9
CoderJava <kolonel.yudisetiawan@gmail.com>
2021-02-14 23:15:19 +0700

    Create feature for addition, subtraction, multiplication, and division

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ const secondColor = Color(0xFF404347);
 const thirdColor = Color(0xFF646669);
 const fourthColor = Color(0xFFFF9F0A);
 
+enum OperasiMatematika {
+  PEMBAGIAN,
+  PERKALIAN,
+  PENGURANGAN,
+  PENJUMLAHAN,
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -29,11 +36,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  final listOperations = <int>[];
-
-  String strNumberResult = '0';
+  double numberA;
+  double numberB;
+  var strNumberResult = '0';
   AnimationController controller;
   Animation<double> animation;
+  OperasiMatematika operasiMatematika;
+  var isButtonOperationActive = false;
 
   @override
   void initState() {
@@ -129,14 +138,55 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             '.',
             thirdColor,
             () {
-              _updateStrNumberResult('.');
+              // TODO: buat fitur tombol koma
+              // _updateStrNumberResult('.');
             },
           ),
           _buildWidgetButtonCalculator(
             '=',
             fourthColor,
             () {
-              // TODO: buat fitur tombol sama dengan
+              if (numberA != null) {
+                numberB = double.parse(strNumberResult.replaceAll(',', ''));
+                switch (operasiMatematika) {
+                  case OperasiMatematika.PEMBAGIAN:
+                    isButtonOperationActive = true;
+                    double hasilPembagian = numberA / numberB;
+                    numberA = null;
+                    numberB = null;
+                    strNumberResult = _formatNumber(hasilPembagian.toString());
+                    operasiMatematika = null;
+                    setState(() {});
+                    break;
+                  case OperasiMatematika.PERKALIAN:
+                    isButtonOperationActive = true;
+                    double hasilPerkalian = numberA * numberB;
+                    numberA = null;
+                    numberB = null;
+                    strNumberResult = _formatNumber(hasilPerkalian.toString());
+                    operasiMatematika = null;
+                    setState(() {});
+                    break;
+                  case OperasiMatematika.PENGURANGAN:
+                    isButtonOperationActive = true;
+                    double hasilPengurangan = numberA - numberB;
+                    numberA = null;
+                    numberB = null;
+                    strNumberResult = _formatNumber(hasilPengurangan.toString());
+                    operasiMatematika = null;
+                    setState(() {});
+                    break;
+                  case OperasiMatematika.PENJUMLAHAN:
+                    isButtonOperationActive = true;
+                    double hasilPenjumlahan = numberA + numberB;
+                    numberA = null;
+                    numberB = null;
+                    strNumberResult = _formatNumber(hasilPenjumlahan.toString());
+                    operasiMatematika = null;
+                    setState(() {});
+                    break;
+                }
+              }
             },
           ),
         ],
@@ -172,10 +222,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ),
           _buildWidgetButtonCalculator(
             '+',
-            fourthColor,
+            operasiMatematika == OperasiMatematika.PENJUMLAHAN ? Colors.white : fourthColor,
             () {
-              // TODO: buat fitur operasi penjumlahan
+              _lakukanPerhitungan(OperasiMatematika.PENJUMLAHAN);
             },
+            textColor: operasiMatematika == OperasiMatematika.PENJUMLAHAN ? fourthColor : Colors.white,
           ),
         ],
       ),
@@ -210,14 +261,61 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ),
           _buildWidgetButtonCalculator(
             '-',
-            fourthColor,
+            operasiMatematika == OperasiMatematika.PENGURANGAN ? Colors.white : fourthColor,
             () {
-              // TODO: buat fitur operasi pengurangan
+              _lakukanPerhitungan(OperasiMatematika.PENGURANGAN);
             },
+            textColor: operasiMatematika == OperasiMatematika.PENGURANGAN ? fourthColor : Colors.white,
           ),
         ],
       ),
     );
+  }
+
+  void _lakukanPerhitungan(OperasiMatematika operasiMatematikaNew) {
+    var number = double.parse(strNumberResult.replaceAll(',', ''));
+    if (operasiMatematika != null && !isButtonOperationActive) {
+      isButtonOperationActive = true;
+      if (numberA == null) {
+        numberA = number;
+      } else if (numberB == null) {
+        numberB = number;
+      }
+      if (numberA != null && numberB != null) {
+        switch (operasiMatematika) {
+          case OperasiMatematika.PEMBAGIAN:
+            double hasilPembagian = numberA / numberB;
+            numberA = hasilPembagian;
+            numberB = null;
+            strNumberResult = _formatNumber(hasilPembagian.toString());
+            break;
+          case OperasiMatematika.PERKALIAN:
+            double hasilPerkalian = numberA * numberB;
+            numberA = hasilPerkalian;
+            numberB = null;
+            strNumberResult = _formatNumber(hasilPerkalian.toString());
+            break;
+          case OperasiMatematika.PENGURANGAN:
+            double hasilPengurangan = numberA - numberB;
+            numberA = hasilPengurangan;
+            numberB = null;
+            strNumberResult = _formatNumber(hasilPengurangan.toString());
+            break;
+          case OperasiMatematika.PENJUMLAHAN:
+            double hasilPenjumlahan = numberA + numberB;
+            numberA = hasilPenjumlahan;
+            numberB = null;
+            strNumberResult = _formatNumber(hasilPenjumlahan.toString());
+            break;
+        }
+      }
+    } else {
+      isButtonOperationActive = true;
+      numberA = number;
+    }
+    operasiMatematika = operasiMatematikaNew;
+    controller.forward(from: 0);
+    setState(() {});
   }
 
   Widget _buildWidgetButtonRow2() {
@@ -248,10 +346,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ),
           _buildWidgetButtonCalculator(
             'x',
-            fourthColor,
+            operasiMatematika == OperasiMatematika.PERKALIAN ? Colors.white : fourthColor,
             () {
-              // TODO: buat fitur operasi perkalian
+              _lakukanPerhitungan(OperasiMatematika.PERKALIAN);
             },
+            textColor: operasiMatematika == OperasiMatematika.PERKALIAN ? fourthColor : Colors.white,
           ),
         ],
       ),
@@ -269,6 +368,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             () {
               setState(() {
                 strNumberResult = '0';
+                operasiMatematika = null;
+                numberA = null;
+                numberB = null;
                 controller.forward(from: 0);
               });
             },
@@ -292,10 +394,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ),
           _buildWidgetButtonCalculator(
             '/',
-            fourthColor,
+            operasiMatematika == OperasiMatematika.PEMBAGIAN ? Colors.white : fourthColor,
             () {
-              // TODO: buat fitur operasi pembagian
+              _lakukanPerhitungan(OperasiMatematika.PEMBAGIAN);
             },
+            textColor: operasiMatematika == OperasiMatematika.PEMBAGIAN ? fourthColor : Colors.white,
           ),
         ],
       ),
@@ -304,7 +407,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   void _updateStrNumberResult(String strNumber) {
     setState(() {
-      if (strNumberResult.length == 1 && strNumberResult[0] == '0') {
+      if (isButtonOperationActive) {
+        strNumberResult = strNumber;
+        isButtonOperationActive = false;
+      } else if (strNumberResult.length == 1 && strNumberResult[0] == '0') {
         if (strNumber == '0') {
           return;
         }
@@ -318,10 +424,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   String _formatNumber(String strNumber) {
     strNumber = strNumber.replaceAll(',', '');
-    bool isNegative = strNumber.startsWith('-');
-    if (isNegative) {
-      strNumber = strNumber.replaceFirst('-', '');
+    double numberDouble = double.parse(strNumber);
+    num number;
+    if (isInteger(numberDouble)) {
+      number = numberDouble.toInt();
+    } else {
+      number = numberDouble;
     }
+    strNumber = number.toString();
     var len = strNumber.length;
     var size = 3;
     var chunks = <String>[];
@@ -331,20 +441,27 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     }
     chunks = chunks.reversed.toList();
     var result = chunks.join(',');
-    if (isNegative) {
-      result = '-$result';
-    }
     return result;
   }
 
-  Widget _buildWidgetButtonCalculator(String label, Color backgroundColor, Function onTap, {int flex = 1}) {
+  bool isInteger(num value) {
+    return value % 1 == 0;
+  }
+
+  Widget _buildWidgetButtonCalculator(
+    String label,
+    Color backgroundColor,
+    Function onTap, {
+    int flex = 1,
+    Color textColor = Colors.white,
+  }) {
     return Expanded(
       flex: flex,
       child: RaisedButton(
         child: Text(
           label,
           style: Theme.of(context).textTheme.headline6.copyWith(
-                color: Colors.white,
+                color: textColor,
               ),
         ),
         onPressed: onTap,
